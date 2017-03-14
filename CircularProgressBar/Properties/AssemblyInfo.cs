268b2a46fcea395f11856c5bcc1d1886adb383ea
s_falahati <s_falahati@hotmail.com>
2017-03-15 02:15:29 +0330

    New Release

--- CircularProgressBar/Properties/AssemblyInfo.cs
+++ CircularProgressBar/Properties/AssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Soroush Falahati")]
 [assembly: AssemblyProduct("Circular ProgressBar")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Soroush Falahati")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.5.*")]
+[assembly: AssemblyFileVersion("2.5")]
