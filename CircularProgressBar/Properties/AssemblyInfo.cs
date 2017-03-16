6a9be3107d7b0a1b33cbc071e89151b8de90734b
s_falahati <s_falahati@hotmail.com>
2017-03-16 19:44:35 +0330

    Fixed a problem with DLL detected as a satellite assembly

--- CircularProgressBar/Properties/AssemblyInfo.cs
+++ CircularProgressBar/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Circular ProgressBar")]
 [assembly: AssemblyCopyright("Soroush Falahati")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
